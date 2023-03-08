c9ebf07bd4b0a935571e88c6065d47bc6fea71ce
spawn22 <todococo14@gmail.com>
2023-03-08 12:18:04 -0300

    queryClient en app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 import Movies from "@/components/Movies";
 import { Navbar } from "@/components";
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 export default function Home() {
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-       
       <Navbar />
       <Movies />
-     </QueryClientProvider>
-      
     </>
   );
 }
